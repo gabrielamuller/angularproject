544ffd5c2bcb9288b5d64b31fb5b5a81ceb57a4a
Gabriela Muller <mullergabrielaa@gmail.com>
2017-09-26 15:59:55 +0000

    day12 commit

--- day12/calctest/src/calc.js
+++ day12/calctest/src/calc.js
@@ -4,7 +4,47 @@ Calculator = function() {
  
 Calculator.prototype.add = function(number) {
     if (typeof(number) == "number") {
+        if(this.value == 0) {
+        this.value = number;
+    } else {
         this.value += number;
+    }
+    } else {
+        alert("Argument must be a number");
+    }
+};
+
+Calculator.prototype.sub = function(number) {
+    if (typeof(number) == "number") {
+        if(this.value == 0) {
+        this.value = number;
+    } else {
+        this.value -= number;
+    }
+    } else {
+        alert("Argument must be a number");
+    }
+};
+
+Calculator.prototype.mult = function(number) {
+    if (typeof(number) == "number") {
+        if(this.value == 0) {
+        this.value = number;
+    } else {
+        this.value *= number;
+    }
+    } else {
+        alert("Argument must be a number");
+    }
+};
+
+Calculator.prototype.div = function(number) {
+    if (typeof(number) == "number") {
+        if(this.value == 0) {
+        this.value = number;
+    } else {
+        this.value /= number;
+    }
     } else {
         alert("Argument must be a number");
     }
